1d573592d82527e357340740662b018e324a7e18
Naveed Shahzad <lhrciit@gmail.com>
2021-09-09 17:08:40 +0500

    updated the assets

--- resources/js/router/router.js
+++ resources/js/router/router.js
@@ -3,7 +3,7 @@ import Home from "../views/Home.vue";
 import SearchResult from "../views/SearchResult.vue";
 
 export const router = createRouter({
-    base: '/rlcos',
+    base: '/rlcos/',
     history: createWebHistory(),
     linkActiveClass: 'text-dark bg-white',
     routes: [
